a6d81e3d02bc62fdd6821407c998515d641cc183
Shantanu2k19 <shantanusingh2030@gmail.com>
2024-03-06 23:48:49 +0530

    working basic lambda

--- AVSynthGen/GPTApis.py
+++ AVSynthGen/GPTApis.py
@@ -1,18 +1,3 @@
-from openai import OpenAI
-
 def CheckGPTConnection():
     print("checking")
-    return 
-
-def GetGPTResponse(prompt):
-    client = OpenAI()
-
-    response = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": prompt},
-    ]
-    )
-    #print(response['choices'][0]['message']['content'].strip())
-    print(response)
+    return {"statusCode": 200, "body": "Hello, World!"} 
